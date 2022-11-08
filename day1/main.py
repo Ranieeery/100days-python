33ddc3f0dbbd8506bad5527c20856841d95b897d
Raniery <102702376+Ranieeery@users.noreply.github.com>
2022-11-08 12:02:52 -0300

    Delete day1 directory

--- day1/main.py
+++
@@ -1,9 +0,0 @@
-print("Raniery Meireles Goulart")
-print("10/21/2022")
-
-print("""I am signing up for Replit's 100 days of Python challenge!
-I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
-I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
-
-print("I am feeling 👻")
-print("You can follow my progress at replit.com/@Ranieery or at https://github.com/Ranieeery/100days-python")
