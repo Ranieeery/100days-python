eb7dd4e558f798b92328b94b6ddce074b133e037
Raniery <102702376+Ranieeery@users.noreply.github.com>
2022-11-08 12:11:58 -0300

    rename folders

---
+++ day02/main.py
@@ -0,0 +1,6 @@
+userName = input("What's your name? ")
+userFavFood = input("What's your favorite food? ")
+userFavMusic = input("What's your favorite music? ")
+userLocation = input("Where your live? ")
+
+print("Hi " + userName + ", You're probably hungry for " + userFavFood + ", you're probably listening to  " + userFavMusic + " and you're probably living in the amazing " + userLocation)
---
+++ day06/main.py
@@ -0,0 +1,11 @@
+userLogin = input("Hi, what is your username? ")
+userPass = input("Please insert your password: ")
+
+if userLogin == "Raniery" and userPass == "r3id3l4s":
+  print("Welcome Raniery!")
+elif userLogin == "Gabrielle" and userPass == "ug4b0g4":
+  print("Welcome Raniery!")
+elif userLogin == "Analice" and userPass == "mnisntana":
+  print("Welcome Raniery!")
+else:
+  print("????? go away bro")
